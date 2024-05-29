0ca5b8ab2c90966ccc9283b04a4c82d959f1deb3
JoshCunningHum <jkdimpas@gmail.com>
2024-05-30 00:29:14 +0800

    Added QR Component
    defaults to current browser location

---
+++ components/QR.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import QrcodeVue from "qrcode.vue";
+import { get } from "@vueuse/core";
+
+const model = defineModel<string>();
+
+// Props
+const { width = 500 } = defineProps<{
+    width?: number;
+}>();
+
+// Fallback to whatever the current link is
+const loc = useBrowserLocation();
+const link = computed(() => get(model) || get(loc).origin || "");
+</script>
+
+<template>
+    <qrcode-vue
+        :value="link"
+        :size="width"
+        level="H"
+        :margin="1"
+        class="rounded"
+    />
+</template>
+
+<style lang="scss" scoped></style>
