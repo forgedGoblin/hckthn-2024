7313b0c30ab77e240ffe77c367c4691e19d329a9
JoshCunningHum <jkdimpas@gmail.com>
2024-05-30 00:21:27 +0800

    Added Timeout Feature for useAsyncState

---
+++ composables/AsyncStateTimeout.ts
@@ -0,0 +1,35 @@
+import { type UseAsyncStateOptions, promiseTimeout } from "@vueuse/core";
+
+interface UseAsyncStateTimeoutOptions<Shallow extends boolean, D = any>
+    extends UseAsyncStateOptions<Shallow, D> {
+    timeout?: number;
+}
+
+export const useAsyncStateTimeout = <
+    Data,
+    Params extends any[] = [],
+    Shallow extends boolean = true,
+>(
+    promise: Promise<Data> | ((...args: Params) => Promise<Data>),
+    initialState: Data,
+    options?: UseAsyncStateTimeoutOptions<Shallow, Data>
+) => {
+    const { timeout } = options ?? {};
+    const asyncstatereturn = useAsyncState(
+        (...args: Params) =>
+            new Promise<Data>(async (resolve, reject) => {
+                const _promise =
+                    typeof promise === "function" ? promise(...args) : promise;
+                _promise
+                    .then((res) => resolve(res))
+                    .catch((err) => reject(err));
+                // another promise for the timeout
+                if (timeout && timeout > 0)
+                    promiseTimeout(timeout).then(() => reject(`Timeout`));
+            }),
+        initialState,
+        options
+    );
+
+    return asyncstatereturn;
+};
